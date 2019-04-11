b5a8b3ec091d0be110fc9147401cd57ad24d5379
Charles Cunningham <charles.cunningham@outlook.com>
2019-04-11 17:31:10 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+{
+    echo `sudo apt update -y`
+    echo `sudo apt upgrade -y`
+
+    echo `apt-get git`
+
+    echo `curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash`
+
+    echo `source ~/.bashrc`
+
+    echo `nvm install 10`
+
+    echo `npm install -g jolocom-cli`
+}
